97f10f7e40a0d12895f05b007b70e24bf0dabe8c
simaoamorim <simao_amorim@outook.pt>
2019-10-07 10:31:13 +0200

    Replace GUI XRC source with python source
    XRC only allows simple widgets to be used. Moved to python GUI source
    for better integration with the rest of the code.
    
    Signed-off-by: simaoamorim <simao_amorim@outook.pt>

--- src/cnc_tooldb.py
+++ src/cnc_tooldb.py
@@ -1,42 +1,52 @@
 #!/usr/bin/env python3
 """
-File Docstring
+CNC ToolDB application manages a CNC Tool database using sqlite3
+and wxPython for the GUI.
 """
 import wx
-import wx.xrc
+import wx.dataview
+import gui
+from wx.adv import AboutBox, AboutDialogInfo
 
 
-class App(wx.App):
-    """
-        Main application class. Here are defined all the methods and
-        functions that control the entire application
-    """
-    def __init__(self, **argv):
-        """Initialize the GUI from the XRC file, import the needed objects and declare variables"""
-        super().__init__(**argv)
-        # Get the main objects from XRC
-        self.res = wx.xrc.XmlResource(filemask="wxformsbuilder/gui.xrc")
-        self.MainFrame = self.res.LoadFrame(parent=None, name='MainFrame')
-        self.SetTopWindow(self.MainFrame)
-        self.machine_chooser = wx.xrc.XRCCTRL(window=self.MainFrame, str_id='machine_chooser')
-        # Declare future variables
-        self.machines = None
-        # Call initialization methods
+class MainFrame(gui.MainFrame):
+    """ Main application class. Here are defined all the methods and
+        functions that control the entire application """
+    def __init__(self):
+        # Set info about the application
+        self.info = AboutDialogInfo()
+        with open('../version.txt', 'r') as version_file:
+            self.info.SetVersion(version_file.read().__str__())
+        self.info.SetName(u"CNC ToolDB")
+        self.info.SetCopyright(u"Copyright (c) 2019 Simão Amorim <simao_amorim@outlook.pt>")
+        with open('../LICENSE', 'r') as license_file:
+            self.info.SetLicence(license_file.read().encode('UTF-8'))
+        self.info.SetDevelopers([u"Simão Amorim (simao_amorim@outlook.pt)"])
+        # Initialize the window
+        super().__init__(parent=None, title=self.info.GetName()+" "+self.info.GetVersion())
         self.init_binds()
-        self.MainFrame.Show()
+        self.Show()
 
     def init_binds(self):
-        """Bind GUI events to the proper methods"""
-        self.MainFrame.Bind(wx.EVT_MENU, self.menu_exit, id=wx.xrc.XRCID(str_id='file_menu_exit'))
+        """Bind GUI events to the appropriate handler methods"""
+        self.Bind(wx.EVT_MENU, self.menubar_handler, self.menu_bar)
+        pass
 
-    def menu_exit(self, event):
+    def menubar_handler(self, event):
+        """Method to handle events from the menu bar in the main frame"""
+        temp_id = event.GetId()
+        if temp_id == self.file_menu_exit.Id:
+            self.Destroy()
+        elif temp_id == self.help_menu_about.Id:
+            AboutBox(info=self.info, parent=self, )
         event.StopPropagation()
-        print('Exiting application...')
-        self.ExitMainLoop()
 
 
 if __name__ == '__main__':
-    root = App(redirect=False)
+    """Initialization steps for the application that are only executed on the
+    main execution"""
+    root = wx.App(redirect=False)
+    root.SetTopWindow(MainFrame())
     ret = root.MainLoop()
     print('GUI exit code: %d' % ret)
     print('Exiting')
